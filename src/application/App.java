83835c3ee847a5e6fededd4456862a542b60d7f9
Bruno Emanuel Pereira <bruno_emanuel87@hotmail.com>
2025-05-16 15:30:42 -0300

    Department entity created

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import model.entities.Department;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    Department dep = new Department(1, "Informatics");
+    System.out.println(dep);
+  }
 }
