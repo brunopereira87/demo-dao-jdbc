175289ace43e192533e38da2267ba5a6c9422839
Bruno Emanuel Pereira <bruno_emanuel87@hotmail.com>
2025-05-16 15:42:10 -0300

    Seller entity created

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
   public static void main(String[] args) throws Exception {
     Department dep = new Department(1, "Informatics");
     System.out.println(dep);
+
+    Seller seller = new Seller(1, "Maria", "d8d5H@example.com", new Date(), 3000.0, dep);
+    System.out.println(seller);
   }
 }
